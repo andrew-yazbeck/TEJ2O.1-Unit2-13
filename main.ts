b0ea13b0e8eb0b2ea65d48a1f6a745be43f5d2c5
andrew-yazbeck <andrew.yazbeck@stu.ocsb.ca>
2025-10-27 20:36:18 -0400

    shows a while loop

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Andrew
+ * Created on: Oct 2025
+ * This program shows a while loop
 */
 
-basic.showString('Hello, World!')
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// p seress "A" button
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    while (loopCounter <= 5) {
+        basic.pause(500)
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// press "B" button
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(5, 5)
+    while (loopCounter >= -1) {
+        basic.pause(500)
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
